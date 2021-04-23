a2967783a09d61e8db6281088b35e0f9d30670d2
Gyeong Dok Ko <42568666+lemidia@users.noreply.github.com>
2021-04-23 22:25:31 +0900

    config modified

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,5 @@
 import firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDscLMhfUwBwUfNQWxLeIxVsmJX8cY-2qM",
-  authDomain: "slack-lemidia.firebaseapp.com",
-  projectId: "slack-lemidia",
-  storageBucket: "slack-lemidia.appspot.com",
-  messagingSenderId: "167370298236",
-  appId: "1:167370298236:web:eee035f81e9bcd1c2f4af9",
-  measurementId: "G-C36JM3PXPL",
-};
+import firebaseConfig from "./firebaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
